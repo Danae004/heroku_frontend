76a3cb80ffe0fa16f995f28e863fc888fa4dfc3d
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-12-01 21:06:22 -0600

    uploads
    Actualizando Contactos

---
+++ static/js/get_editar.js
@@ -0,0 +1,63 @@
+function editarContacto() {
+    const correo_actual = document.getElementById('correo_actual').value;
+    const nuevoNombre = document.getElementById('nombre').value;
+    const nuevoTelefono = document.getElementById('telefono').value;
+
+    // Validar campos si es necesario
+
+    const URL = "http://127.0.0.1:8000/contactos/" + correo_actual;
+
+    var request = new XMLHttpRequest();
+    request.open('PUT', URL, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+
+    var datosActualizados = {
+        email: correo_actual,
+        nombre: nuevoNombre,
+        telefono: nuevoTelefono
+    };
+
+    request.send(JSON.stringify(datosActualizados));
+
+    request.onload = () => {
+        if (request.status === 200) {
+            // Contacto actualizado correctamente
+            console.log("Contacto actualizado exitosamente");
+
+            // Actualizar el campo de correo actual con el nuevo correo
+            document.getElementById('correo_actual').value = correo_actual;
+
+            // Puedes mostrar un mensaje si es necesario
+            document.getElementById('mensaje').innerHTML = "Contacto actualizado exitosamente";
+        } else if (request.status === 404) {
+            // No se encontró el contacto con ese correo
+            console.log("No se encontró el contacto con ese correo.");
+            // Puedes mostrar un mensaje si es necesario
+            document.getElementById('mensaje').innerHTML = "No se encontró el contacto con ese correo.";
+        } else {
+            // Error al intentar actualizar el contacto
+            console.log("Error al intentar actualizar el contacto.");
+            // Puedes mostrar un mensaje si es necesario
+            document.getElementById('mensaje').innerHTML = "Error al intentar actualizar el contacto.";
+        }
+    };
+}
+
+function cancelarEdicion() {
+    // Puedes redirigir al usuario a otra página o realizar cualquier acción necesaria al cancelar la edición
+    console.log("Edición cancelada");
+}
+
+function regresar() {
+    // Redirige al usuario al índice (index.html)
+    window.location.href = "/";  // Ajusta la ruta según sea necesario
+}
+
+
+
+
+
+
+
+
+
