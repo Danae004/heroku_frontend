10c93373673324af2b5baebd34fb0fa487840c6c
Danae004 <122801105+Danae004@users.noreply.github.com>
2023-12-01 22:18:09 -0600

    Update get_all.js
    ---

--- static/js/get_all.js
+++ static/js/get_all.js
@@ -1,5 +1,5 @@
 function getAll() {
-    const URL = "https://backend-contactos-c554215d4ff0.herokuapp.com";
+    const URL = "https://backend-contactos-c554215d4ff0.herokuapp.com/contactos";
     var request = new XMLHttpRequest();
     request.open('GET', URL);
     request.send();
